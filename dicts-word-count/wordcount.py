5d78631d055f64f216fc7a421f0bf297ad1915f5
Aleena <aleenataymoor@gmail.com>
2021-07-28 00:40:20 -0700

    Dictionary word count assignment 2nd commit

--- dicts-word-count/wordcount.py
+++ dicts-word-count/wordcount.py
@@ -1,20 +1,26 @@
+
+
+
+
 import string
-# import sys
+import sys 
 
-# print(sys.argv)
+parameterList = sys.argv
+for i in range(1, len(parameterList)):
+    param = parameterList[i]
+    def word_counter(file_name):    
+        open_file=open(file_name,"r")
+        word_count={}
+        for line in open_file:
+            line = line.rstrip()
+            for character in string.punctuation:
+                line = line.replace(character,"")
+            line = line.lower()
+            words = line.split(" ")
+            for word in words:
+                word_count[word] = word_count.get(word, 0) + 1
+        return word_count
 
-def word_counter(file_name="test.txt"):
-    open_file=open(file_name,"r")
-    word_count={}
-    for line in open_file:
-        line = line.rstrip()
-        for character in string.punctuation:
-            line = line.replace(character,"")
-        line = line.lower()
-        words = line.split(" ")
-        for word in words:
-            word_count[word] = word_count.get(word, 0) + 1
-    return word_count
-        
-result = word_counter()
-print(result)
+    result = word_counter(param)
+    print(result)
+    print("----------------------------------------------------")
